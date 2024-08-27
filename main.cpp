3d8e33457e0157368a9c78394bc2f695e9172248
ka6aH4ukk <agadaagadaagada69@gmail.com>
2024-08-28 01:03:10 +0200

    Initial project version

---
+++ main.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+using namespace std;
+
+
+	char arr[10][10] {
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'}
+			};
+
+				
+void clear() {
+    cout << "\x1B[2J\x1B[H";
+}
+
+void matrix() {
+	for (int i = 0; i < 10; i++) {
+			cout<<arr[0][i];
+			cout<<arr[1][i];
+			cout<<arr[2][i];
+			cout<<arr[3][i];
+			cout<<arr[4][i];
+			cout<<arr[5][i];
+			cout<<arr[6][i];
+			cout<<arr[7][i];
+			cout<<arr[8][i];
+			cout<<arr[9][i]<<endl;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	
+		
+	matrix();
+	
+	int x = 0;
+	int y = 0;
+	int c = 0;
+	int a = 0;
+	int xe = 9;
+	int ye = 9;
+	int q = 0;
+	
+	arr[xe][ye] = '@';
+	arr[x][y] = '#';
+	
+	clear();
+	
+	matrix();
+	
+	for(;;) {
+		clear();
+		matrix();
+		arr[xe][ye] = '0';
+		
+		q++;
+		int rx = x - xe;
+		int ry = y - ye;
+		int irx = rx;
+		int iry = ry;
+		if (rx < 0) {
+			irx=rx*-1;
+		}
+		if (ry < 0) {
+			iry=ry*-1;
+		}
+		if (q%2 == 0) {
+			if (irx < iry) {
+				if (ry < 0) {
+					ye--;
+				}
+				else {
+					ye++;
+				}
+			}
+			else {
+				if (rx < 0) {
+					xe--;
+				}
+				else {
+					xe++;
+				}
+			}
+		}
+
+		arr[x][y] = '0';
+		cout<<"letter: ";
+		cin>>c;
+		switch(c) {
+			case 1:
+				if ( x == 0) {
+					cout<<"nope";
+				}
+				else {
+					x=x-1;
+				}
+				break;
+			case 2:
+				if (x == 9) {
+					cout<<"nope";
+				}
+				else {
+					x=x+1;
+				}
+				break;
+			case 3:
+				if (y == 0) {
+					cout<<"nope";
+				}
+				else {
+					y--;
+				}
+				break;
+			case 4:
+				if (y == 9) {
+					cout<<"nope";
+				}
+				else {
+					y++;
+				}
+				break;
+		}
+		
+		arr[xe][ye] = '@';
+		arr[x][y] = '#';
+		clear();
+		if (arr[x][y] == arr[xe][ye]) {
+                        cout<<"САС";
+                        cin>>a;
+                        break;
+                }
+	}
+}
+
